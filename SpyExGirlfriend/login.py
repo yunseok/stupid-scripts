f1adce4af19f84b6e0dafeebc452574816cc5d13
yunseok <yunseok.noreply@gmail.com>
2019-07-23 19:45:55 +0200

    Fixed the Instagram Script (#2)

---
+++ SpyExGirlfriend/login.py
@@ -0,0 +1,6 @@
+import subprocess
+
+# It will ask you to enter your password, after it's done, you can execute elizabeth.py!
+username = 'anant.sisou'
+
+subprocess.run(['instaloader', '--login', username])
